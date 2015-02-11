3b247a1f70ee0015b16e9849548c26ce6868d55e
Brian Capps <Brian.Capps@nytimes.com>
2015-02-11 15:39:19 -0500

    copies NYTScalingImageView from core

---
+++ Pod/Classes/ios/NYTScalingImageView.h
@@ -0,0 +1,27 @@
+//
+//  NYTScalingImageView.h
+//  Pods
+//
+//  Created by Harrison, Andrew on 7/23/13.
+//  Copyright (c) 2015 The New York Times Company. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NYTScalingImageView : UIScrollView
+
+@property (nonatomic) UIImageView *internalImageView;
+
+/** Initializes a scaling image view with a UIImage. This object is a UIScrollView that contains a UIImageView. This allows for zooming and panning around the image. Additionally it supports double tap to zoom.
+ 
+ @param UIImage A UIImage object.
+ @param CGRect  The frame to initialize the NYTScalingImageView in.
+ @return Whatever it returns.
+ */
+- (instancetype)initWithImage:(UIImage *)image frame:(CGRect)frame;
+
+/** Centers the image inside of the scroll view. Typically used after rotation, or when zooming has finished.
+ */
+- (void)centerScrollViewContents;
+
+@end
---
+++ Pod/Classes/ios/NYTScalingImageView.m
@@ -0,0 +1,90 @@
+//
+//  NYTScalingImageView.m
+//  Pods
+//
+//  Created by Harrison, Andrew on 7/23/13.
+//  Copyright (c) 2015 The New York Times Company. All rights reserved.
+//
+
+#import "NYTScalingImageView.h"
+
+#import "tgmath.h"
+
+@implementation NYTScalingImageView
+
+#pragma mark - UIView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    return [self initWithImage:nil frame:frame];
+}
+
+- (void)didAddSubview:(UIView *)subview {
+    [super didAddSubview:subview];
+    [self centerScrollViewContents];
+}
+
+- (void)setFrame:(CGRect)frame {
+    [super setFrame:frame];
+    [self centerScrollViewContents];
+}
+
+#pragma mark - NYTScalingImageView
+
+- (id)initWithImage:(UIImage *)image frame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self setupInternalImageView:image];
+        [self setupImageScrollView];
+    }
+    
+    return self;
+}
+
+#pragma mark - Setup
+
+- (void)setupInternalImageView:(UIImage *)image {
+    self.internalImageView = [[UIImageView alloc] initWithImage:image];
+    self.internalImageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    [self addSubview:self.internalImageView];
+    
+    self.contentSize = image.size;
+}
+
+- (void)setupImageScrollView {
+    self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.showsVerticalScrollIndicator = NO;
+    self.showsHorizontalScrollIndicator = NO;
+    self.bouncesZoom = YES;
+    self.decelerationRate = UIScrollViewDecelerationRateFast;
+}
+
+#pragma mark - Centering
+
+#define floor __tg_floor //This is to address the Clang bug discussed here http://stackoverflow.com/questions/23333287/tgmath-h-doesnt-work-if-modules-are-enabled
+
+- (void)centerScrollViewContents {
+    CGFloat horizontalInset = 0;
+    CGFloat verticalInset = 0;
+
+    if (self.contentSize.width < CGRectGetWidth(self.bounds)) {
+        horizontalInset = (CGRectGetWidth(self.bounds) - self.contentSize.width) * 0.5;
+    }
+    
+    if (self.contentSize.height < CGRectGetHeight(self.bounds)) {
+        verticalInset = (CGRectGetHeight(self.bounds) - self.contentSize.height) * 0.5;
+    }
+    
+    if (self.window.screen.scale < 2.0) {
+        horizontalInset = floor(horizontalInset);
+        verticalInset = floor(verticalInset);
+    }
+    
+    // Use contentInset to center the contents in the scrollview. Reasoning explained here: http://petersteinberger.com/blog/2013/how-to-center-uiscrollview/
+    self.contentInset = UIEdgeInsetsMake(verticalInset, horizontalInset, verticalInset, horizontalInset);
+}
+
+#undef floor
+
+@end
