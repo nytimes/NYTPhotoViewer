e442f36b41143573160b6ec9f61d7c246d63a5e9
Brian Capps <Brian.Capps@nytimes.com>
2015-02-17 22:55:05 -0500

    Removes old constants.

--- Pod/Classes/ios/NYTPhotosViewController.m
+++ Pod/Classes/ios/NYTPhotosViewController.m
@@ -15,9 +15,6 @@
 #import "NYTPhoto.h"
 #import "NYTPhotosOverlayView.h"
 
-const CGFloat NYTPhotosViewControllerPanDismissDistanceRatio = 100.0/667.0; // distance over iPhone 6 height.
-const CGFloat NYTPhotosViewControllerPanDismissMaximumDuration = 0.45;
-
 @interface NYTPhotosViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate, NYTPhotoViewControllerDelegate>
 
 @property (nonatomic) id <NYTPhotosViewControllerDataSource> dataSource;
