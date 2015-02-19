7f499360b88ea492a3a43365c8e6919ee57f6889
Brian Capps <Brian.Capps@nytimes.com>
2015-02-19 17:40:10 -0500

    Docs on NYTPhotosOverlayView

--- Pod/Classes/ios/NYTPhotosOverlayView.h
+++ Pod/Classes/ios/NYTPhotosOverlayView.h
@@ -10,12 +10,29 @@
 
 @interface NYTPhotosOverlayView : UIView
 
+/**
+ *  The internal navigation bar used to set the bar button items and title of the overlay.
+ */
 @property (nonatomic, readonly) UINavigationBar *navigationBar;
 
+/**
+ *  The title of the overlay. Centered between the eft adn right bar button items.
+ */
 @property (nonatomic) NSString *title;
+
+/**
+ *  The bar button item appearing at the top left of the overlay.
+ */
 @property (nonatomic) UIBarButtonItem *leftBarButtonItem;
+
+/**
+ *  The bar button item appearing at the top right of the overlay.
+ */
 @property (nonatomic) UIBarButtonItem *rightBarButtonItem;
 
+/**
+ *  A view representing the caption for the photo, which will be set to full width and locked to the bottom. Can be any UIView object, but is expected to respond to `intrinsicContentSize` appropriately to calculate height.
+ */
 @property (nonatomic) UIView *captionView;
 
 @end
