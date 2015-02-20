e54c8c28fba05df5aa1de303e5dd19125698e62a
Brian Capps <Brian.Capps@nytimes.com>
2015-02-20 10:52:45 -0500

    Docs for NYTPhoto

--- Pod/Classes/ios/Protocols/NYTPhoto.h
+++ Pod/Classes/ios/Protocols/NYTPhoto.h
@@ -8,16 +8,39 @@
 
 @import UIKit;
 
+/**
+ *  The model for photos displayed in an `NYTPhotosViewController`.
+ */
 @protocol NYTPhoto <NSObject>
 
+/**
+ *  The image to display.
+ */
 @property (nonatomic) UIImage *image;
 
+/**
+ *  A placeholder image for display while the image is loading.
+ */
 @property (nonatomic) UIImage *placeholderImage;
 
+/**
+ *  An attributed string for display as the title of the caption.
+ */
 @property (nonatomic) NSAttributedString *attributedCaptionTitle;
+
+/**
+ *  An attributed string for display as the summary of the caption.
+ */
 @property (nonatomic) NSAttributedString *attributedCaptionSummary;
+
+/**
+ *  An attributed string for display as the credit of the caption.
+ */
 @property (nonatomic) NSAttributedString *attributedCaptionCredit;
 
+/**
+ *  An identifying string to distinguish photos objects and compare equality.
+ */
 @property (nonatomic) NSString *identifier;
 
 @end
