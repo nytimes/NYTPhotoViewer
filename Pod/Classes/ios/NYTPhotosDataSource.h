101cef13942d9d49b635e06258e3471fde4b7313
Brian Capps <Brian.Capps@nytimes.com>
2015-02-19 23:40:17 -0500

    Docs for NYTPhotosDataSource

--- Pod/Classes/ios/NYTPhotosDataSource.h
+++ Pod/Classes/ios/NYTPhotosDataSource.h
@@ -10,8 +10,18 @@
 
 #import "NYTPhotosViewControllerDataSource.h"
 
+/**
+ *  A concrete implementation of the NYTPhotosViewControllerDataSource.
+ */
 @interface NYTPhotosDataSource : NSObject <NYTPhotosViewControllerDataSource>
 
+/**
+ *  The designated initializer that takes and stores an array of photos.
+ *
+ *  @param photos An array of objects conforming to the NYTPhoto protocol.
+ *
+ *  @return A fully initialized object.
+ */
 - (instancetype)initWithPhotos:(NSArray *)photos NS_DESIGNATED_INITIALIZER;
 
 @end
