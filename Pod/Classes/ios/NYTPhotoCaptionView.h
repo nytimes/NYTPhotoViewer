0ece2a4f4be0ebe47034af0db3729ff25225e2eb
Brian Capps <Brian.Capps@nytimes.com>
2015-02-20 10:55:14 -0500

    Docs for NYTPhotoCaptionView

--- Pod/Classes/ios/NYTPhotoCaptionView.h
+++ Pod/Classes/ios/NYTPhotoCaptionView.h
@@ -10,10 +10,30 @@
 
 @interface NYTPhotoCaptionView : UIView
 
+/**
+ *  The attributed string used as the title. The top string in the caption view.
+ */
 @property (nonatomic) NSAttributedString *attributedTitle;
+
+/**
+ *  The attributed string used as the summary. The second from the top string in the caption view.
+ */
 @property (nonatomic) NSAttributedString *attributedSummary;
+
+/**
+ *  The attributed string used as the credit. The third from the top string in the caption view.
+ */
 @property (nonatomic) NSAttributedString *attributedCredit;
 
+/**
+ *  Designated initializer that takes all the attributed strings as arguments.
+ *
+ *  @param attributedTitle   The attributed string used as the title. The top string in the caption view.
+ *  @param attributedSummary The attributed string used as the summary. The second from the top string in the caption view.
+ *  @param attributedCredit  The attributed string used as the credit. The third from the top string in the caption view.
+ *
+ *  @return A fully initialized object.
+ */
 - (instancetype)initWithAttributedTitle:(NSAttributedString *)attributedTitle attributedSummary:(NSAttributedString *)attributedSummary attributedCredit:(NSAttributedString *)attributedCredit NS_DESIGNATED_INITIALIZER;
 
 @end
