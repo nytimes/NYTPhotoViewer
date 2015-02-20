7d41beff12bd94b6816b142fa64abf38fd43d3b6
Brian Capps <Brian.Capps@nytimes.com>
2015-02-20 10:47:30 -0500

    Docs for NYTPhotoTransitionAnimator

--- Pod/Classes/ios/NYTPhotoTransitionAnimator.h
+++ Pod/Classes/ios/NYTPhotoTransitionAnimator.h
@@ -10,14 +10,39 @@
 
 @interface NYTPhotoTransitionAnimator : NSObject <UIViewControllerAnimatedTransitioning>
 
+/**
+ *  The view from which to start an image zooming transition. This view may be hidden or show in the transition, but will never be removed or changed in its view hierarchy.
+ */
 @property (nonatomic) UIView *startingView;
+
+/**
+ *  The view from which to end an image zooming transition. This view may be hidden or show in the transition, but will never be removed or changed in its view hierarchy.
+ */
+
 @property (nonatomic) UIView *endingView;
 
+/**
+ *  The view that is used for animating the starting view. If no view is set, the starting view is screenshotted and relevant properties copied to the new view.
+ */
 @property (nonatomic) UIView *startingViewForAnimation;
+
+/**
+ *  The view that is used for animating the ending view. If no view is set, the ending view is screenshotted and relevant properties copied to the new view.
+ */
 @property (nonatomic) UIView *endingViewForAnimation;
 
+/**
+ *  Whether this transition is a dismissal. If NO, presentation is assumed.
+ */
 @property (nonatomic, getter=isDismissing) BOOL dismissing;
 
+/**
+ *  Convenience method for creating a view for animation from another arbitrary view. Attempts to create an identical view in the most efficient way possible.
+ *
+ *  @param view The view to create the animation from.
+ *
+ *  @return A new view identical in apperance to the passed-in view, with relevant properties transferred. Not a member of any view hierarchy.
+ */
 + (UIView *)newAnimationViewFromView:(UIView *)view;
 
 @end
