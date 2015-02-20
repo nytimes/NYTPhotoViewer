93bf33cc0d81e7ad4847e4fccc5df6545dcf68b4
Brian Capps <Brian.Capps@nytimes.com>
2015-02-20 11:10:43 -0500

    Docs for NYTPhotoViewController

--- Pod/Classes/ios/NYTPhotoViewController.h
+++ Pod/Classes/ios/NYTPhotoViewController.h
@@ -19,16 +19,46 @@
  */
 extern NSString * const NYTPhotoViewControllerPhotoImageUpdatedNotification;
 
+/**
+ *  The view controller controlling the display of a single photo object.
+ */
 @interface NYTPhotoViewController : UIViewController <NYTPhotoContaining>
 
+/**
+ *  The object that acts as the photo view controller's delegate.
+ */
 @property (nonatomic, weak) id <NYTPhotoViewControllerDelegate> delegate;
 
+/**
+ *  The internal scaling image view used to display the photo.
+ */
 @property (nonatomic, readonly) NYTScalingImageView *scalingImageView;
+
+/**
+ *  The internal activity view shown while the image is loading. Set form the initializer.
+ */
 @property (nonatomic, readonly) UIView *activityView;
+
+/**
+ *  The gesture recognizer used to detect the double tap gesture used for zooming on photos.
+ */
 @property (nonatomic, readonly) UITapGestureRecognizer *doubleTapGestureRecognizer;
 
+/**
+ *  The designated initializer that takes the photo and activity view.
+ *
+ *  @param photo        The photo object that this view controller manages.
+ *  @param activityView The activity view to display while the photo's image loads. Call `updateImage:` with a non-nil image to hide.
+ *
+ *  @return A fully initialized object.
+ */
 - (instancetype)initWithPhoto:(id <NYTPhoto>)photo activityView:(UIView *)activityView NS_DESIGNATED_INITIALIZER;
 
+/**
+ *  Updates the image displayed for the photo. If previously showing an activity view, that view is hidden and removed.
+ *
+ *  @param image The new image to display.
+ */
 - (void)updateImage:(UIImage *)image;
 
 @end
@@ -36,6 +66,13 @@ extern NSString * const NYTPhotoViewControllerPhotoImageUpdatedNotification;
 @protocol NYTPhotoViewControllerDelegate <NSObject>
 
 @optional
+
+/**
+ *  Called when a long press is recognized.
+ *
+ *  @param photoViewController        The `NYTPhotoViewController` instance that sent the delegate message.
+ *  @param longPressGestureRecognizer The long press gesture recognizer that recognized the long press.
+ */
 - (void)photoViewController:(NYTPhotoViewController *)photoViewController didLongPressWithGestureRecognizer:(UILongPressGestureRecognizer *)longPressGestureRecognizer;
 
 @end
