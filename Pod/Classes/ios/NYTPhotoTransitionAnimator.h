d18d8874bc9f8937dafc56be449ede1260e19860
Brian Capps <Brian.Capps@nytimes.com>
2015-02-21 15:25:40 -0500

    One more bit of clarity to animation view docs

--- Pod/Classes/ios/NYTPhotoTransitionAnimator.h
+++ Pod/Classes/ios/NYTPhotoTransitionAnimator.h
@@ -43,7 +43,7 @@
  *
  *  @param view The view to create the animation from.
  *
- *  @return A new view identical in apperance to the passed-in view, with relevant properties transferred. Not a member of any view hierarchy.
+ *  @return A new view identical in apperance to the passed-in view, with relevant properties transferred. Not a member of any view hierarchy. Return nil if the passed-in view is nil.
  */
 + (UIView *)newAnimationViewFromView:(UIView *)view;
 
