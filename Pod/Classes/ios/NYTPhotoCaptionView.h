602966d1665db6f2be07824ff567e2f3e352049e
Brian Capps <Brian.Capps@nytimes.com>
2015-02-25 17:37:57 -0500

    Moves unused public properties to private interface

--- Pod/Classes/ios/NYTPhotoCaptionView.h
+++ Pod/Classes/ios/NYTPhotoCaptionView.h
@@ -19,22 +19,7 @@ extern const CGFloat NYTPhotoCaptionViewHorizontalMargin;
 @interface NYTPhotoCaptionView : UIView
 
 /**
- *  The attributed string used as the title. The top string in the caption view.
- */
-@property (nonatomic) NSAttributedString *attributedTitle;
-
-/**
- *  The attributed string used as the summary. The second from the top string in the caption view.
- */
-@property (nonatomic) NSAttributedString *attributedSummary;
-
-/**
- *  The attributed string used as the credit. The third from the top string in the caption view. Added directly after the summary, if one exists.
- */
-@property (nonatomic) NSAttributedString *attributedCredit;
-
-/**
- *  Designated initializer that takes all the attributed strings as arguments.
+ *  Designated initializer that takes all the caption attributed strings as arguments.
  *
  *  @param attributedTitle   The attributed string used as the title. The top string in the caption view.
  *  @param attributedSummary The attributed string used as the summary. The second from the top string in the caption view.
--- Pod/Classes/ios/NYTPhotoCaptionView.m
+++ Pod/Classes/ios/NYTPhotoCaptionView.m
@@ -13,6 +13,10 @@ static const CGFloat NYTPhotoCaptionViewVerticalMargin = 10.0;
 
 @interface NYTPhotoCaptionView ()
 
+@property (nonatomic) NSAttributedString *attributedTitle;
+@property (nonatomic) NSAttributedString *attributedSummary;
+@property (nonatomic) NSAttributedString *attributedCredit;
+
 @property (nonatomic) UILabel *textLabel;
 @property (nonatomic) CAGradientLayer *gradientLayer;
 
@@ -71,24 +75,6 @@ static const CGFloat NYTPhotoCaptionViewVerticalMargin = 10.0;
     self.gradientLayer.frame = self.layer.bounds;
     self.gradientLayer.colors = [NSArray arrayWithObjects:(id)[UIColor clearColor].CGColor, (id)[[UIColor blackColor] colorWithAlphaComponent:0.85].CGColor, nil];
     [self.layer insertSublayer:self.gradientLayer atIndex:0];
-}
-
-- (void)setAttributedTitle:(NSAttributedString *)attributedTitle {
-    _attributedTitle = attributedTitle;
-    
-    [self updateTextLabelAttributedText];
-}
-
-- (void)setAttributedSummary:(NSAttributedString *)attributedSummary {
-    _attributedSummary = attributedSummary;
-    
-    [self updateTextLabelAttributedText];
-}
-
-- (void)setAttributedCredit:(NSAttributedString *)attributedCredit {
-    _attributedCredit = attributedCredit;
-    
-    [self updateTextLabelAttributedText];
 }
 
 - (void)updateTextLabelAttributedText {
