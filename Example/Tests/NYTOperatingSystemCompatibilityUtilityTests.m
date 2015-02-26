fa119771015913b90b34e798f21a572e986bb86a
Brian Capps <Brian.Capps@nytimes.com>
2015-02-26 11:00:41 -0500

    Adds test file shells

---
+++ Example/Tests/NYTOperatingSystemCompatibilityUtilityTests.m
@@ -0,0 +1,24 @@
+//
+//  NYTOperatingSystemCompatibilityUtilityTests.m
+//  NYTPhotoViewer
+//
+//  Created by Brian Capps on 2/26/15.
+//  Copyright (c) 2015 Brian Capps. All rights reserved.
+//
+
+@import UIKit;
+@import XCTest;
+
+#import <NYTPhotoViewer/NYTOperatingSystemCompatibilityUtility.h>
+
+@interface NYTOperatingSystemCompatibilityUtilityTests : XCTestCase
+
+@end
+
+@implementation NYTOperatingSystemCompatibilityUtilityTests
+
+- (void)testExample {
+    XCTAssertTrue(YES);
+}
+
+@end
---
+++ Example/Tests/NYTScalingImageViewTests.m
@@ -0,0 +1,24 @@
+//
+//  NYTScalingImageViewTests.m
+//  NYTPhotoViewer
+//
+//  Created by Brian Capps on 2/26/15.
+//  Copyright (c) 2015 Brian Capps. All rights reserved.
+//
+
+@import UIKit;
+@import XCTest;
+
+#import <NYTPhotoViewer/NYTScalingImageView.h>
+
+@interface NYTScalingImageViewTests : XCTestCase
+
+@end
+
+@implementation NYTScalingImageViewTests
+
+- (void)testExample {
+    XCTAssertTrue(YES);
+}
+
+@end
