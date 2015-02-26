7512a339f12bd0cd9d49a5acc6edf2e804350e76
Brian Capps <Brian.Capps@nytimes.com>
2015-02-26 11:24:11 -0500

    Adds OCMock and some mocks to NYTOperatingSystemCompatibilityUtilityTests

--- Example/Tests/NYTOperatingSystemCompatibilityUtilityTests.m
+++ Example/Tests/NYTOperatingSystemCompatibilityUtilityTests.m
@@ -9,6 +9,7 @@
 @import UIKit;
 @import XCTest;
 
+#import <OCMock/OCMock.h>
 #import <NYTPhotoViewer/NYTOperatingSystemCompatibilityUtility.h>
 
 @interface NYTOperatingSystemCompatibilityUtilityTests : XCTestCase
@@ -17,8 +18,32 @@
 
 @implementation NYTOperatingSystemCompatibilityUtilityTests
 
-- (void)testExample {
-    XCTAssertTrue(YES);
+- (void)testiOS8toViewMethodCalledOniOS8 {
+    id partialMock = OCMPartialMock([UIDevice currentDevice]);
+    OCMStub((NSString *)[(UIDevice *)partialMock systemVersion]).andCall(self, @selector(iOS8String));
+    
+    id transitionContext = OCMProtocolMock(@protocol(UIViewControllerContextTransitioning));
+    
+    UIView *testView = [[UIView alloc] init];
+    OCMStub([transitionContext viewForKey:OCMOCK_ANY]).andReturn(testView);
+    
+    XCTAssertEqualObjects(testView, [NYTOperatingSystemCompatibilityUtility toViewForTransitionContext:transitionContext]);
+}
+
+- (void)testiOS8fromViewMethodCalledOniOS8 {
+    id partialMock = OCMPartialMock([UIDevice currentDevice]);
+    OCMStub((NSString *)[(UIDevice *)partialMock systemVersion]).andCall(self, @selector(iOS8String));
+    
+    id transitionContext = OCMProtocolMock(@protocol(UIViewControllerContextTransitioning));
+    
+    UIView *testView = [[UIView alloc] init];
+    OCMStub([transitionContext viewForKey:OCMOCK_ANY]).andReturn(testView);
+    
+    XCTAssertEqualObjects(testView, [NYTOperatingSystemCompatibilityUtility fromViewForTransitionContext:transitionContext]);
+}
+
+- (NSString *)iOS8String {
+    return @"8.0.0";
 }
 
 @end
