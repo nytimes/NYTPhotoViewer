0369c4438d659bb095d7a5481a0dffd58f4628d2
Brian Capps <Brian.Capps@nytimes.com>
2015-03-04 10:08:35 -0500

    Adds landscape versions and renames

--- Pod/Classes/ios/NYTPhotosViewController.m
+++ Pod/Classes/ios/NYTPhotosViewController.m
@@ -150,7 +150,7 @@ static const UIEdgeInsets NYTPhotosViewControllerDefaultLeftBarButtonImageInsets
         
         // iOS 7 has an issue with constraints that could evaluate to be negative, so we set the width to the margins' size.
         _overlayView = [[NYTPhotosOverlayView alloc] initWithFrame:CGRectMake(0, 0, NYTPhotoCaptionViewHorizontalMargin * 2.0, 0)];
-        _overlayView.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"NYTPhotoViewer.bundle/imgViewerX"] landscapeImagePhone:[UIImage imageNamed:@"NYTPhotoViewer.bundle/imgViewerX"] style:UIBarButtonItemStylePlain target:self action:@selector(doneButtonTapped:)];
+        _overlayView.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"NYTPhotoViewer.bundle/NYTPhotoViewerCloseButtonX"] landscapeImagePhone:[UIImage imageNamed:@"NYTPhotoViewer.bundle/NYTPhotoViewerCloseButtonXLandscape"] style:UIBarButtonItemStylePlain target:self action:@selector(doneButtonTapped:)];
         _overlayView.leftBarButtonItem.imageInsets = NYTPhotosViewControllerDefaultLeftBarButtonImageInsets;
         _overlayView.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionButtonTapped:)];
 
