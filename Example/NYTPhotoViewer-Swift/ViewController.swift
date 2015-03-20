1834f28279a12b3cac38152422efabdf90fbafb0
Mark Paul Keefe <mark@maynebridge.com>
2015-03-20 14:54:41 -0400

    New targets

---
+++ Example/NYTPhotoViewer-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYTPhotoViewer-Swift
+//
+//  Created by Mark Keefe on 3/20/15.
+//  Copyright (c) 2015 Brian Capps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
