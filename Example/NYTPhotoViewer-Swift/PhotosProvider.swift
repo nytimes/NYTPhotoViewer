3268d4594d1901a93a330d4667ccb447dfab872b
Mark Paul Keefe <mark@maynebridge.com>
2015-03-20 19:42:27 -0400

    Connects PhotosProvider datasource

--- Example/NYTPhotoViewer-Swift/PhotosProvider.swift
+++ Example/NYTPhotoViewer-Swift/PhotosProvider.swift
@@ -10,29 +10,28 @@ import UIKit
 
 class PhotosProvider: NSObject {
     
-    static let NumberOfPhotos = 5
-    static let CustomEverythingPhotoIndex = 1
-    static let DefaultLoadingSpinnerPhotoIndex = 3
-    static let NoReferenceViewPhotoIndex = 4
-    static let SummaryString = "summary"
-    static let CreditString = "credit"
-
     static let photos: [ExamplePhoto] = {
         
-        var mutablePhotos: [ExamplePhoto] = []
+        let NumberOfPhotos = 5
+        let SummaryString = "summary", CreditString = "credit"
         let image = UIImage(named: "testImage"), placeholderImage = UIImage(named: "testImagePlaceholder")
+
         let attributedCaptionSummary = NSAttributedString(string: SummaryString, attributes: [NSForegroundColorAttributeName: UIColor.grayColor()])
         let attributedCaptionCredit = NSAttributedString(string: CreditString, attributes: [NSForegroundColorAttributeName: UIColor.darkGrayColor()])
         
         func shouldSetImageOnIndex(photoIndex: Int) -> Bool {
-            return photoIndex == CustomEverythingPhotoIndex || photoIndex == DefaultLoadingSpinnerPhotoIndex
+            let customEverythingPhotoIndex = 1, defaultLoadingSpinnerPhotoIndex = 3
+            return photoIndex == customEverythingPhotoIndex || photoIndex == defaultLoadingSpinnerPhotoIndex
         }
+
+        var mutablePhotos: [ExamplePhoto] = []
 
         for var photoIndex = 0; photoIndex < NumberOfPhotos; photoIndex++ {
 
+            let image = shouldSetImageOnIndex(photoIndex) ? image : nil
             let attributedCaptionTitle = NSAttributedString(string: "\(photoIndex + 1)", attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
 
-            let photo = ExamplePhoto(image: shouldSetImageOnIndex(photoIndex) ? image : nil, placeholderImage: placeholderImage, attributedCaptionTitle: attributedCaptionTitle, attributedCaptionSummary: attributedCaptionSummary, attributedCaptionCredit: attributedCaptionCredit)
+            let photo = ExamplePhoto(image: image, placeholderImage: placeholderImage, attributedCaptionTitle: attributedCaptionTitle, attributedCaptionSummary: attributedCaptionSummary, attributedCaptionCredit: attributedCaptionCredit)
         
             mutablePhotos.append(photo)
         }
