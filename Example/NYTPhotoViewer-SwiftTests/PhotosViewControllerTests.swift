12c3808e8ef5d1ee15205d22fe2663efc6980433
Mark Paul Keefe <mark@maynebridge.com>
2015-03-21 15:54:17 -0400

    NYTPhotoDataSource tests

--- Example/NYTPhotoViewer-SwiftTests/PhotosViewControllerTests.swift
+++ Example/NYTPhotoViewer-SwiftTests/PhotosViewControllerTests.swift
@@ -1,16 +1,15 @@
 //
-//  NYTPhotoViewer_SwiftTests.swift
-//  NYTPhotoViewer-SwiftTests
+//  PhotosViewControllerTests.swift
+//  NYTPhotoViewer
 //
-//  Created by Mark Keefe on 3/20/15.
+//  Created by Mark Keefe on 3/21/15.
 //  Copyright (c) 2015 The New York Times. All rights reserved.
 //
 
-import UIKit
 import XCTest
 
-class NYTPhotoViewerSwiftTests: XCTestCase {
-    
+class PhotosViewControllerTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +19,17 @@ class NYTPhotoViewerSwiftTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
