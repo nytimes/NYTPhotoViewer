68d59953b456b1402149f9d9c69688467e59b6e2
Mark Paul Keefe <mark@maynebridge.com>
2015-03-21 11:55:37 -0400

    Swift model conforms to protocol declared in ObjC (fixes bridging header issue)

--- Example/NYTPhotoViewer-Swift/NYTPhotoViewer-Swift-Bridging-Header.h
+++ Example/NYTPhotoViewer-Swift/NYTPhotoViewer-Swift-Bridging-Header.h
@@ -2,4 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <NYTPhotoViewer/NYTPhoto.h>
 #import <NYTPhotoViewer/NYTPhotosViewController.h>
--- Example/NYTPhotoViewer-Swift/ViewController.swift
+++ Example/NYTPhotoViewer-Swift/ViewController.swift
@@ -11,7 +11,35 @@ import UIKit
 class ViewController: UIViewController, NYTPhotosViewControllerDelegate {
 
     @IBOutlet weak var imageButton : UIButton!
-    static private let photos = PhotosProvider.photos
+    private let photos = PhotosProvider.photos
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        
+        let photosViewController = NYTPhotosViewController(photos: photos as [AnyObject]!)
+        photosViewController.delegate = self
+        presentViewController(photosViewController, animated: true, completion: nil)
+        
+        updateImagesOnPhotosViewController(photosViewController, afterDelayWithPhotos: photos)
+    }
+    
+    func updateImagesOnPhotosViewController(photosViewController: NYTPhotosViewController, afterDelayWithPhotos: [ExamplePhoto]) {
+        let updateImageDelay: Int64 = 5
+        let imageName = "testImage"
+
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            for photo in self.photos {
+                if photo.image == nil {
+                    photo.image = UIImage(named: imageName)
+                    photosViewController.updateImageForPhoto(photo)
+                }
+            }
+        }
+    }
+    
+    // MARK: - NYTPhotosViewControllerDelegate
+    
     
 }
 
