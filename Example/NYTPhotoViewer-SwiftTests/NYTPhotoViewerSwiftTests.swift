1487dfc063b271ff86a6aca8b27c062d088ec1d7
Mark Paul Keefe <mark@maynebridge.com>
2015-03-28 19:20:41 -0400

    Rebase in progress 2...

---
+++ Example/NYTPhotoViewer-SwiftTests/NYTPhotoViewerSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  NYTPhotoViewer_SwiftTests.swift
+//  NYTPhotoViewer-SwiftTests
+//
+//  Created by Mark Keefe on 3/20/15.
+//  Copyright (c) 2015 Brian Capps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NYTPhotoViewerSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
