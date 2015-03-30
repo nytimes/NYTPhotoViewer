6ea18c5b4186b98d481879377adb823c138d7a24
Mark Paul Keefe <mark@maynebridge.com>
2015-03-30 15:06:02 -0400

    Adds remaining files generated by pod install

---
+++ Example/Pods/Target Support Files/Pods-NYTPhotoViewer-SwiftTests-OCMock/Pods-NYTPhotoViewer-SwiftTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NYTPhotoViewer_SwiftTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_NYTPhotoViewer_SwiftTests_OCMock
+@end
---
+++ Example/Pods/Target Support Files/Pods-NYTPhotoViewer-SwiftTests/Pods-NYTPhotoViewer-SwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NYTPhotoViewer_SwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_NYTPhotoViewer_SwiftTests
+@end
