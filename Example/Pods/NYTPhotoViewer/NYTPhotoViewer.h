4853b7bd95644197077edb6cfeac0e22c47fe4a8
Michael Selsky <michael.selsky@nytimes.com>
2015-07-15 16:36:43 -0400

    Sharing Schemes

---
+++ Example/Pods/NYTPhotoViewer/NYTPhotoViewer.h
@@ -0,0 +1,19 @@
+//
+//  NYTPhotoViewer.h
+//  NYTPhotoViewer
+//
+//  Created by Selsky, Michael on 7/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NYTPhotoViewer.
+FOUNDATION_EXPORT double NYTPhotoViewerVersionNumber;
+
+//! Project version string for NYTPhotoViewer.
+FOUNDATION_EXPORT const unsigned char NYTPhotoViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYTPhotoViewer/PublicHeader.h>
+
+
