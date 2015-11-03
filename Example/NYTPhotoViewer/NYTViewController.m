4acbeebcbf0eecfd7cbf4617efd6182d2cbc0432
Chris Dzombak <chris@chrisdzombak.net>
2015-11-03 09:23:11 -0500

    Use an enum for example photo indices
    per feedback https://github.com/NYTimes/NYTPhotoViewer/pull/88/files#r43697798

--- Example/NYTPhotoViewer/NYTViewController.m
+++ Example/NYTPhotoViewer/NYTViewController.m
@@ -10,11 +10,13 @@
 #import <NYTPhotoViewer/NYTPhotosViewController.h>
 #import "NYTExamplePhoto.h"
 
-static const NSUInteger NYTViewControllerCustomEverythingPhotoIndex = 1;
-static const NSUInteger NYTViewControllerLongCaptionPhotoIndex = 2;
-static const NSUInteger NYTViewControllerDefaultLoadingSpinnerPhotoIndex = 3;
-static const NSUInteger NYTViewControllerNoReferenceViewPhotoIndex = 4;
-static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
+typedef NS_ENUM(NSUInteger, NYTViewControllerPhotoIndex) {
+    NYTViewControllerPhotoIndexCustomEverything = 1,
+    NYTViewControllerPhotoIndexLongCaption = 2,
+    NYTViewControllerPhotoIndexDefaultLoadingSpinner = 3,
+    NYTViewControllerPhotoIndexNoReferenceView = 4,
+    NYTViewControllerPhotoIndexCustomMaxZoomScale = 5,
+};
 
 @interface NYTViewController () <NYTPhotosViewControllerDelegate>
 
@@ -56,29 +58,31 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
         NYTExamplePhoto *photo = [[NYTExamplePhoto alloc] init];
         
         photo.image = [UIImage imageNamed:@"NYTimesBuilding"];
-        if (i == NYTViewControllerCustomEverythingPhotoIndex || i == NYTViewControllerDefaultLoadingSpinnerPhotoIndex) {
+        if (i == NYTViewControllerPhotoIndexCustomEverything || i == NYTViewControllerPhotoIndexDefaultLoadingSpinner) {
             photo.image = nil;
         }
         
-        if (i == NYTViewControllerCustomEverythingPhotoIndex) {
+        if (i == NYTViewControllerPhotoIndexCustomEverything) {
             photo.placeholderImage = [UIImage imageNamed:@"NYTimesBuildingPlaceholder"];
         }
 
         NSString *caption = @"summary";
-        if (i == NYTViewControllerCustomEverythingPhotoIndex) {
-            caption = @"photo with custom everything";
-        }
-        else if (i == NYTViewControllerLongCaptionPhotoIndex) {
-            caption = @"photo with long caption. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus laoreet vehicula. Maecenas elit quam, pellentesque at tempor vel, tempus non sem. Vestibulum ut aliquam elit. Vivamus rhoncus sapien turpis, at feugiat augue luctus id. Nulla mi urna, viverra sed augue malesuada, bibendum bibendum massa. Cras urna nibh, lacinia vitae feugiat eu, consectetur a tellus. Morbi venenatis nunc sit amet varius pretium. Duis eget sem nec nulla lobortis finibus. Nullam pulvinar gravida est eget tristique. Curabitur faucibus nisl eu diam ullamcorper, at pharetra eros dictum. Suspendisse nibh urna, ultrices a augue a, euismod mattis felis. Ut varius tortor ac efficitur pellentesque. Mauris sit amet rhoncus dolor. Proin vel porttitor mi. Pellentesque lobortis interdum turpis, vitae tincidunt purus vestibulum vel. Phasellus tincidunt vel mi sit amet congue.";
-        }
-        else if (i == NYTViewControllerDefaultLoadingSpinnerPhotoIndex) {
-            caption = @"photo with loading spinner";
-        }
-        else if (i == NYTViewControllerNoReferenceViewPhotoIndex) {
-            caption = @"photo without reference view";
-        }
-        else if (i == NYTViewControllerCustomMaxZoomScalePhotoIndex) {
-            caption = @"photo with custom maximum zoom scale";
+        switch ((NYTViewControllerPhotoIndex)i) {
+            case NYTViewControllerPhotoIndexCustomEverything:
+                caption = @"photo with custom everything";
+                break;
+            case NYTViewControllerPhotoIndexLongCaption:
+                caption = @"photo with long caption. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus laoreet vehicula. Maecenas elit quam, pellentesque at tempor vel, tempus non sem. Vestibulum ut aliquam elit. Vivamus rhoncus sapien turpis, at feugiat augue luctus id. Nulla mi urna, viverra sed augue malesuada, bibendum bibendum massa. Cras urna nibh, lacinia vitae feugiat eu, consectetur a tellus. Morbi venenatis nunc sit amet varius pretium. Duis eget sem nec nulla lobortis finibus. Nullam pulvinar gravida est eget tristique. Curabitur faucibus nisl eu diam ullamcorper, at pharetra eros dictum. Suspendisse nibh urna, ultrices a augue a, euismod mattis felis. Ut varius tortor ac efficitur pellentesque. Mauris sit amet rhoncus dolor. Proin vel porttitor mi. Pellentesque lobortis interdum turpis, vitae tincidunt purus vestibulum vel. Phasellus tincidunt vel mi sit amet congue.";
+                break;
+            case NYTViewControllerPhotoIndexDefaultLoadingSpinner:
+                caption = @"photo with loading spinner";
+                break;
+            case NYTViewControllerPhotoIndexNoReferenceView:
+                caption = @"photo without reference view";
+                break;
+            case NYTViewControllerPhotoIndexCustomMaxZoomScale:
+                caption = @"photo with custom maximum zoom scale";
+                break;
         }
         
         photo.attributedCaptionTitle = [[NSAttributedString alloc] initWithString:@(i + 1).stringValue attributes:@{NSForegroundColorAttributeName: [UIColor whiteColor], NSFontAttributeName: [UIFont preferredFontForTextStyle:UIFontTextStyleBody]}];
@@ -94,7 +98,7 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
 #pragma mark - NYTPhotosViewControllerDelegate
 
 - (UIView *)photosViewController:(NYTPhotosViewController *)photosViewController referenceViewForPhoto:(id <NYTPhoto>)photo {
-    if ([photo isEqual:self.photos[NYTViewControllerNoReferenceViewPhotoIndex]]) {
+    if ([photo isEqual:self.photos[NYTViewControllerPhotoIndexNoReferenceView]]) {
         return nil;
     }
     
@@ -102,7 +106,7 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
 }
 
 - (UIView *)photosViewController:(NYTPhotosViewController *)photosViewController loadingViewForPhoto:(id <NYTPhoto>)photo {
-    if ([photo isEqual:self.photos[NYTViewControllerCustomEverythingPhotoIndex]]) {
+    if ([photo isEqual:self.photos[NYTViewControllerPhotoIndexCustomEverything]]) {
         UILabel *loadingLabel = [[UILabel alloc] init];
         loadingLabel.text = @"Custom Loading...";
         loadingLabel.textColor = [UIColor greenColor];
@@ -113,7 +117,7 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
 }
 
 - (UIView *)photosViewController:(NYTPhotosViewController *)photosViewController captionViewForPhoto:(id <NYTPhoto>)photo {
-    if ([photo isEqual:self.photos[NYTViewControllerCustomEverythingPhotoIndex]]) {
+    if ([photo isEqual:self.photos[NYTViewControllerPhotoIndexCustomEverything]]) {
         UILabel *label = [[UILabel alloc] init];
         label.text = @"Custom Caption View";
         label.textColor = [UIColor whiteColor];
@@ -125,7 +129,7 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
 }
 
 - (CGFloat)photosViewController:(NYTPhotosViewController *)photosViewController maximumZoomScaleForPhoto:(id <NYTPhoto>)photo {
-    if ([photo isEqual:self.photos[NYTViewControllerCustomMaxZoomScalePhotoIndex]]) {
+    if ([photo isEqual:self.photos[NYTViewControllerPhotoIndexCustomMaxZoomScale]]) {
         return 10.0f;
     }
 
@@ -133,7 +137,7 @@ static const NSUInteger NYTViewControllerCustomMaxZoomScalePhotoIndex = 5;
 }
 
 - (NSDictionary *)photosViewController:(NYTPhotosViewController *)photosViewController overlayTitleTextAttributesForPhoto:(id <NYTPhoto>)photo {
-    if ([photo isEqual:self.photos[NYTViewControllerCustomEverythingPhotoIndex]]) {
+    if ([photo isEqual:self.photos[NYTViewControllerPhotoIndexCustomEverything]]) {
         return @{NSForegroundColorAttributeName: [UIColor grayColor]};
     }
     
