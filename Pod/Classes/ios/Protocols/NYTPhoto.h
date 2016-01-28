043299bbb94dbb3340c5ea13ced805b5875d220d
Chris Dzombak <chris@chrisdzombak.net>
2016-01-28 11:22:54 -0500

    Clarify `NYTPhoto` docs about `image` vs `imageData`

--- Pod/Classes/ios/Protocols/NYTPhoto.h
+++ Pod/Classes/ios/Protocols/NYTPhoto.h
@@ -17,17 +17,24 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  *  The image to display.
+ *
+ *  This property is used if and only if `-imageData` returns `nil`, but note that using `UIImage`s whenever possible will result in better performance. See `-imageData`'s documentation for discussion.
  */
 @property (nonatomic, readonly, nullable) UIImage *image;
 
 /**
- * The image data to display. This will be preferred over the `image` property.
- * In case this is empty `image` will be used. The main advantage of using this is animated gif support.
+ *  The image data to display.
+ *  
+ *  This property's value is preferred over `-image`, if `imageData` is non-`nil`. This allows clients to provide image data for FLAnimatedImage when the library is compiled with `ANIMATED_GIF_SUPPORT` defined.
+ *
+ *  Note that if you're working with a non-animated image, using a native `UIImage` will provide better performance. Therefore, it is recommended to return `nil` from this property unless this photo is an animated GIF.
  */
 @property (nonatomic, readonly, nullable) NSData *imageData;
 
 /**
  *  A placeholder image for display while the image is loading.
+ *
+ *  This property is used if and only if `-imageData` returns `nil`.
  */
 @property (nonatomic, readonly, nullable) UIImage *placeholderImage;
 
