4d0e782bac4c25fa914237758d596fe9414dc5c5
teffi <steffi.tan07@gmail.com>
2016-08-04 21:18:28 +0800

    Added sample for solid background title.

--- Example/NYTViewController.m
+++ Example/NYTViewController.m
@@ -35,6 +35,8 @@ typedef NS_ENUM(NSUInteger, NYTViewControllerPhotoIndex) {
     NYTPhotosViewController *photosViewController = [[NYTPhotosViewController alloc] initWithPhotos:self.photos];
     photosViewController.delegate = self;
     photosViewController.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor], NSFontAttributeName : [UIFont fontWithName:@"Arial" size:20.0]};
+    photosViewController.isTitleBackgroundSolid = YES;
+    
     [self presentViewController:photosViewController animated:YES completion:nil];
     
     [self updateImagesOnPhotosViewController:photosViewController afterDelayWithPhotos:self.photos];
