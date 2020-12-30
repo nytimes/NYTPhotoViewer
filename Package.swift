77fd4b28344d3a0e773514b1158be6f96520c2ad
Eric Slosser <eric.slosser@nytimes.com>
2020-12-30 16:41:55 -0500

    rearrange resources and headers, add SPM support

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "NYTPhotoViewer",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "NYTPhotoViewer",
+            targets: ["NYTPhotoViewer"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.1")
+    ],
+    targets: [
+        .target(
+            name: "NYTPhotoViewer",
+            dependencies: ["PINRemoteImage"],
+            path: "NYTPhotoViewer",
+            exclude: [
+                "Info.plist"
+            ],
+            resources: [
+                .copy("Resources"),
+            ],
+            cSettings: [
+                .headerSearchPath("./include"),
+            ]),
+//        .testTarget(
+//            name: "NYTPhotoViewerTests",
+//            dependencies: ["NYTPhotoViewer"],
+//            path: "NYTPhotoViewerTests",
+//            exclude: [
+//                "Info.plist"
+//            ])
+    ]
+)
